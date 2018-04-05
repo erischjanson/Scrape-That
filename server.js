98001cdb50c9c4a685516b93ed113523a0557fbb
Elizabeth <erischjanson@gmail.com>
2018-04-05 16:25:45 -0500

    working server

--- server.js
+++ server.js
@@ -1,46 +1,39 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-var path = require("path");
+var logger = require("morgan");
+var mongoose = require("mongoose");
 
+// Our scraping tools
+// Axios is a promised-based http library, similar to jQuery's Ajax method
+// It works on the client and on the server
+var axios = require("axios");
+var cheerio = require("cheerio");
 
-var PORT = process.env.PORT || 3000;
+// Require all models
+var db = require("./models");
 
+var PORT = 3000;
+
+// Initialize Express
 var app = express();
 
-// Set Handlebars.
-var exphbs = require("express-handlebars");
+// Configure middleware
 
-app.engine("handlebars", exphbs({
-    defaultLayout: "main"
-}));
-app.set("view engine", "handlebars");
-
-
-// Requiring our models for syncing
-// var db = require("./models");
-
-// Serve static content for the app from the "public" directory in the application directory.
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Use body-parser for handling form submissions
+app.use(bodyParser.urlencoded({ extended: true }));
+// Use express.static to serve the public folder as a static directory
 app.use(express.static("public"));
 
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
+// By default mongoose uses callbacks for async queries, we're setting it to use promises (.then syntax) instead
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect("mongodb://localhost/scraper");
 
-// parse application/json
-app.use(bodyParser.json());
+// Routes
 
-
-
-// Routes.
-
-// Syncing sequelize models and then starting Express
-// db.sequelize.sync().then(function () {
-//     app.listen(PORT, function () {
-//         console.log("App now listening at localhost:" + PORT);
-//     });
-// });
-
-app.listen(PORT, function(){
-    console.log("App now listening at localhost:" + PORT);
+// Start the server
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT + "!");
 });
