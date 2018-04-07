0a656ade17773abfebb42bb5a818054609d0747b
Elizabeth <erischjanson@gmail.com>
2018-04-07 09:18:23 -0500

    working scrape

---
+++ ernie.js
@@ -0,0 +1,14 @@
+// ERNIE
+
+// function get(Uri)
+// {
+// 	return axios.get(Uri).data
+// }
+
+// SERVER
+// app.get("/scrape", function(req, res){
+  
+
+//     var $ = cheerio.load(ernie.get("http://www.lemonde.fr"));
+
+// }
